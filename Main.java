42a6a41097f933c3f69f06d13e3ac3b0f22b9fe1
chenganglist <1464125055@qq.com>
2016-08-23 11:33:34 +0800

    网易构造队列题目更新

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main{
@@ -8,32 +9,30 @@ public class Main{
         Scanner in = new Scanner(System.in);
 
         int groupNumber = in.nextInt();
-        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        ArrayList<ArrayDeque<Integer>> result = new ArrayList<ArrayDeque<Integer>>();
         for(int j = 0; j < groupNumber; j++){
             int count = in.nextInt();
-            List<Integer> list = new ArrayList<>();
-            for(int i = 0; i < count; i++){
-                list.add(i + 1);
+            Deque<Integer> list = new ArrayDeque<Integer>();
+            for(int i = count; i >=1; i--){
+                list.addFirst(i);
+                int tmp = list.getLast();
+                list.removeLast();
+                list.addFirst(tmp);
             }
-
-            for(int i = 1; i < count; i++){
-                int temp = list.get(count - 1);
-                list.add(count - i - 1, temp);
-                list.remove(list.size() - 1);
-            }
-            result.add((ArrayList<Integer>) list);
+            result.add((ArrayDeque<Integer>) list);
         }
         
-		Iterator<ArrayList<Integer>> it = result.iterator();
+		Iterator<ArrayDeque<Integer>> it = result.iterator();
 		while(it.hasNext())
 		{
-			ArrayList<Integer> a =  it.next();
-			int len = a.size();
-			for(int i=0;i<len-1;i++)
+			ArrayDeque<Integer> a =  it.next();
+			while( a.size()> 1)
 			{
-				System.out.print(a.get(i)+" ");
+				int tmp = a.getFirst();
+				a.removeFirst();
+				System.out.print(tmp+" ");
 			}
-			System.out.println(a.get(len-1));
+			System.out.println((int)a.getFirst());
 		}
         in.close();
     }
